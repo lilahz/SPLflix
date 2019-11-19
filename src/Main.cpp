97a5f41267c0d5fd5583e46800a59dac82b89410
lilahz <lilahz05@gmail.com>
2019-11-19 15:19:29 +0200

    try 1

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <fstream>
 #include "../include/Session.h"
+#include "../include/json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
+
+/* Function to Read given Json file and add information to contect vector
+ *
+ * @param json file
+ */
+void readAndCreate(json& j){
+    for (json::iterator it = j.begin(); it != j.end(); ++it){
+        std::cout << *it << '\n';
+    }
+}
 
 int main(int argc, char** argv){
-	if(argc!=2)
-	{
-		cout << "usage splflix input_file" << endl;
-		return 0;
-	}
-	Session s(argv[1]);
-	s.start();
+//	if(argc!=2)
+//	{
+//		cout << "usage splflix input_file" << endl;
+//		return 0;
+//	}
+//	Session s(argv[1]);
+//	s.start();
+
+    std::ifstream i(argv[1], std::ifstream::binary);
+    //std::ifstream i("config1.json");
+    json j;
+    i >> j;
+    readAndCreate(j);
 	return 0;
 }
+
