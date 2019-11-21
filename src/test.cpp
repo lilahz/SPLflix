224f6bd8b602ae7a58a0b4c9dfaac7cf242a1ed5
lilahz <lilahz05@gmail.com>
2019-11-21 13:14:42 +0200

    test files

---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lilachzi@wincs.cs.bgu.ac.il on 21/11/2019.
+//
+
+#include "test.h"
---
+++ src/test.h
@@ -0,0 +1,14 @@
+//
+// Created by lilachzi@wincs.cs.bgu.ac.il on 21/11/2019.
+//
+
+#ifndef SPLFLIX_TEST_H
+#define SPLFLIX_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //SPLFLIX_TEST_H
